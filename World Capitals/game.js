1cbf6b5e4249f76802912aa0f14b3f026ebe93fa
tomes2000 <aavash2000@gmail.com>
2023-04-25 19:59:19 +0100

    adding in the quiz section

--- World Capitals/game.js
+++ World Capitals/game.js
@@ -6,16 +6,29 @@ const easyDifficulty = "https://opentdb.com/api.php?amount=10&category=22&diffic
 const mediumDifficulty = "https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiple"
 const hardDifficulty = "https://opentdb.com/api.php?amount=10&category=22&difficulty=hard&type=multiple"
 
-// DIFFICULTY BUTTONS
+// Difficulty selection
 const easy = document.getElementById("easy");
 const medium = document.getElementById("medium");
 const hard = document.getElementById("hard");
 let apiAddress;
 
-// GAME PAGE
+// Game Page
 // Scores & Question Number
 let questionCounter = 1;
-let questionNumber = document.getElementById("answer-no");
+let questionNumber = document.getElementById("answer-number");
 let score = 0;
 let acceptingAnswer = true;
 let scoreCounter = document.getElementById("score")
+
+// Quiz
+const question = document.getElementById("question");
+const answerButtons = document.getElementById("answer-text");
+const answer1 = document.getElementById("answer1");
+const answer2 = document.getElementById("answer2");
+const answer3 = document.getElementById("answer3");
+const answer4 = document.getElementById("answer4");
+let correctAnswer;
+const next = document.getElementById("next");
+let answerSelected;
+
+
