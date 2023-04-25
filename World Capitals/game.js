bccebf986140e425e454df98b01266deb349a921
tomes2000 <aavash2000@gmail.com>
2023-04-25 19:46:44 +0100

    implementing difficult buttons onto quiz

--- World Capitals/game.js
+++ World Capitals/game.js
@@ -6,3 +6,9 @@ const easyDifficulty = "https://opentdb.com/api.php?amount=10&category=22&diffic
 const mediumDifficulty = "https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiple"
 const hardDifficulty = "https://opentdb.com/api.php?amount=10&category=22&difficulty=hard&type=multiple"
 
+// DIFFICULTY BUTTONS
+const easy = document.getElementById("easy");
+const medium = document.getElementById("medium");
+const hard = document.getElementById("hard");
+let apiAddress;
+
