6c720853bede94e0d44f2fbeb14ca5a93b085a65
tomes2000 <aavash2000@gmail.com>
2023-04-25 19:46:18 +0100

    implementing geography API

--- World Capitals/game.js
+++ World Capitals/game.js
@@ -1,3 +1,8 @@
 let data = {};
 let questionNumber = 0;
 
+// API
+const easyDifficulty = "https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple"
+const mediumDifficulty = "https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=multiple"
+const hardDifficulty = "https://opentdb.com/api.php?amount=10&category=22&difficulty=hard&type=multiple"
+
