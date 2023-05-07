3a3183fc847f2df9a6500e6c734d89e3a5a5dafe
tomes2000 <aavash2000@gmail.com>
2023-05-07 21:05:11 +0100

    implementing question function with final score

--- World Capitals/game.js
+++ World Capitals/game.js
@@ -89,7 +89,7 @@ function nextQuestion(eventVariable) {
   getQuestion(data);
 }
 
-// Check the next question function
+  // Check the next question function
 function checkAnswer(eventVariable) {
 
   // If answer is clicked on, then the answer button is disabled to prevent re-selection of answers
@@ -115,16 +115,15 @@ function checkAnswer(eventVariable) {
   next.addEventListener("click", nextQuestion);
 }
 
-// Get question function
+/// Get question function
 function getQuestion(data) {
   next.classList.add("hide");
   document.getElementById("outer-container").classList.remove("correct", "incorrect");
-}
 
 // Interact with answer button
-$('.answer-text').prop('disabled', false);
-let results = data.results[questionNumber];
-if (questionNumber <= 14) {
+  $('.answer-text').prop('disabled', false);
+  let results = data.results[questionNumber];
+  if (questionNumber <= 14) {
   question.innerHTML = results.quesiton;
   correctAnswer = results.correctAnswer;
 
@@ -134,5 +133,25 @@ if (questionNumber <= 14) {
   answer2.innerHTML = `${answers[1]}`;
   answer3.innerHTML = `${answers[2]}`;
   answer4.innerHTML = `${answers[3]}`;
-}
 
+// Check for correct answer and add attribute 
+
+  for (let button of answerButtons) {
+  if (button.innerHTML === decodeHtmlEntity(correctAnswer)) {
+    button.setAttribute("data-correct", "true");
+  }
+
+// Adds event listener to each button
+  button.setAttribute("click", checkAnswer);
+}
+  questionNumber++;
+} 
+
+// Final screen with score board
+  else {
+  document.getElementById("quiz-area").classList.add("hide");
+  document.getElementById("end-area").classList.remove("hide");
+
+  finalScore.innerText = `${score}`;
+  }
+}
